ea50ddc4cf22a39640c21b3abd9e1226aa27d6f8
REBECADASA <rebecallumiquinga@gmail.com>
2016-07-12 23:59:08 -0500

    Update Libros.php

--- application/controllers/Libros.php
+++ application/controllers/Libros.php
@@ -1,5 +1,5 @@
 <?php 
-class Vehiculos extends CI_Controller{
+class Libros extends CI_Controller{
  
     function __construct(){
         parent::__construct(); 
@@ -10,30 +10,34 @@ class Vehiculos extends CI_Controller{
      
    function index(){
 
+    
+    
      if($this->session->userdata('perfil')=='ADMINISTRADOR') {
         $crud = new grocery_crud();        
         $crud->set_theme('datatables');
          $crud->unset_export();
         $crud->unset_print(); 
-        $crud->set_table('vehiculo'); 
-        $crud->set_subject('Mis Vehículos');
+        $crud->set_table('libro'); //vehiculo
+        $crud->set_subject('Libros');
 
         //Campos Obligatorios al momento de guardar/editar
-        $crud->required_fields('ID_SUC','ID_VER','ANIO_VEH','MOTOR_VEH','PLACA_VEH','MARCA_VEH','COLOR_VEH','ACCESORIOS_VEH','FOTO_VEH');
-        $crud->columns('ID_SUC','ID_VER','ANIO_VEH','MOTOR_VEH','PLACA_VEH','MARCA_VEH','COLOR_VEH','ACCESORIOS_VEH');
+        $crud->required_fields('ID_SUC','ID_VER','ANIO_LIB','TIPO_LIB','TITULO_LIB','EDITORIAL_LIB','AUTOR_LIB','PAGINAS_LIB','FOTO_LIB' );
+        $crud->columns('ID_SUC','ID_VER','ANIO_LIB','TIPO_LIB','TITULO_LIB','EDITORIAL_LIB','AUTOR_LIB','PAGINAS_LIB');
 
 
 
        //Titulos
-       $crud->display_as('ID_VER','Version');
-       $crud->display_as('ANIO_VEH','Año');
+       $crud->display_as('ID_VER','Version');//Version
+       $crud->display_as('ANIO_LIB','Año');
        $crud->display_as('ID_SUC','Sucursal');  
-       $crud->display_as('ID_VEH','Version');
-       $crud->display_as('MOTOR_VEH','Motor');          
-       $crud->display_as('PLACA_VEH','Placa');          
-       $crud->display_as('MARCA_VEH','Marca'); 
-       $crud->display_as('COLOR_VEH','Color'); 
-       $crud->display_as('ACCESORIOS_VEH','Accesorios');
+       $crud->display_as('ID_LIB','Libro');//Version
+       $crud->display_as('TIPO_LIB','Tipo'); //Motor         
+       $crud->display_as('TITULO_LIB','Titulo');  //Placa 
+       $crud->display_as('EDITORIAL_LIB','Editorial');  //Marca  
+
+      
+       $crud->display_as('AUTOR_LIB','Autor'); //Color
+        $crud->display_as('PAGINAS_LIB','Paginas');  //Accesorios
      
          
        $crud->set_relation('ID_SUC','sucursal','Ciudad: {CIUDAD_SUC}');
